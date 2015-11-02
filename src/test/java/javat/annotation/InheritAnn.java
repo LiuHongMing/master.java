cf518ce4ade746ac0fb613c1519acb4cacb7b4c5
jason.liuhongming <jason.liuhongming@gmail.com>
2015-11-02 18:37:04 +0800

    Source code

--- src/test/java/javat/annotation/InheritAnn.java
+++ src/test/java/javat/annotation/InheritAnn.java
@@ -4,10 +4,11 @@ import java.lang.annotation.*;
 
 public class InheritAnn {
 
-    @Inherited
+    @Inherited // 允许子类继承父类中的注解
     @Target(ElementType.TYPE)
     @Retention(RetentionPolicy.RUNTIME)
     @Documented
-    @interface InheritUp {}
+    @interface InheritUp {
+    }
 
 }
