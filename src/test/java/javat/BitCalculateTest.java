b653fc47cfc4eff28eca3c8949c6bd9add88a6b3
jason.liuhongming <jason.liuhongming@gmail.com>
2015-12-15 18:33:49 +0800

    Refactor

--- src/test/java/javat/BitCalculateTest.java
+++ src/test/java/javat/BitCalculateTest.java
@@ -8,7 +8,10 @@ public class BitCalculateTest {
         System.out.println(i);
         // 返回符号位：负数=-1，0=0，正数=1
         System.out.println(Integer.signum(-10) + "," + Integer.signum(0) + "," + Integer.signum(10));
-        System.out.println(Integer.toBinaryString(65536));
+        // 返回在指定的 int 值中最高位（最左边）的 1 位的位置的值
+        System.out.println(Integer.toBinaryString(125) + ":" + Integer.highestOneBit(125));
+        // 返回在指定的 int 值中最低位（最右边）的 1 位的位置的值
+        System.out.println(Integer.toBinaryString(125) + ":" + Integer.lowestOneBit(125));
     }
 
 }
